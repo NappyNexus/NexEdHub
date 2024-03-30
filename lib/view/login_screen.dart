832a8dac15913bc00eabc14f6df88fc29ecd413e
NappyNexus <gabrielsoriano172@gmail.com>
2024-03-30 07:18:46 -0400

    made some advances with appbar

--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 //import 'package:nexedhub/view/styles.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LogInScreen extends StatefulWidget {
   const LogInScreen({super.key});
@@ -13,12 +14,27 @@ class _LogInScreenState extends State<LogInScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        toolbarHeight: 10,
         elevation: 1,
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.blue,
         shadowColor: Colors.black,
-        leading: Image.asset(
-          "assets/Asset1.png",
-          height: 5,
+        leading: Padding(
+          padding: const EdgeInsets.all(1),
+          child: Container(
+            height: 10,
+            width: 10,
+            decoration: const BoxDecoration(color: Colors.black),
+            child: Image.asset(
+              "assets/Asset1.png",
+            ),
+          ),
+        ),
+        title: Text(
+          "NexEdHub",
+          style: GoogleFonts.montserrat(
+            textStyle: const TextStyle(
+                color: Colors.black, fontWeight: FontWeight.w900, fontSize: 5),
+          ),
         ),
       ),
     );
