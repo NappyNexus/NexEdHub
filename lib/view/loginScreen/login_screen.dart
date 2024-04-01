cfd52765077cf3e4632ba89c0d7382f47aa586ad
NappyNexus <gabrielsoriano172@gmail.com>
2024-04-01 04:02:27 -0400

    Made the general textfield

--- lib/view/loginScreen/login_screen.dart
+++ lib/view/loginScreen/login_screen.dart
@@ -1,7 +1,10 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 //import 'package:nexedhub/view/styles.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:nexedhub/view/loginScreen/components/login_image.dart';
+import 'package:nexedhub/view/loginScreen/components/login_textfield.dart';
 
 class LogInScreen extends StatefulWidget {
   const LogInScreen({super.key});
@@ -11,6 +14,9 @@ class LogInScreen extends StatefulWidget {
 }
 
 class _LogInScreenState extends State<LogInScreen> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,14 +36,54 @@ class _LogInScreenState extends State<LogInScreen> {
           ),
         ),
       ),
-      body: const Center(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 50,
-            ),
-            LogInImage(),
-          ],
+      body: Center(
+        child: SizedBox(
+          width: 500,
+          child: Column(
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              LogInImage(),
+              SizedBox(
+                height: 50,
+              ),
+              Text(
+                "Iniciar Sesión en NexEdHub",
+                style: GoogleFonts.montserrat(
+                  textStyle: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w800,
+                    fontSize: 25,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Nombre de usuario",
+                  style: GoogleFonts.montserrat(
+                    textStyle: const TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              MyTextField(
+                controller: emailController,
+                hintText: "Ingrese su nombre de usuario",
+                obscureText: false,
+              )
+            ],
+          ),
         ),
       ),
     );
